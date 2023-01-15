d7bde37c3ca69927a224c1b183a3177740ed03b4
Mirakbar Mirzokhidov <mirakbarmirzokhidov21@gmail.com>
2023-01-15 20:27:14 +0600

    fix: variables

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task4;
-
+import java.util.Scanner;
 public class FormulaCreator {
     public static void main(String[] args) {
+
+        Scanner obj = new Scanner(System.in);
+        float a = obj.nextFloat();
+        float b = obj.nextFloat();
+        System.out.println((9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2));
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,81 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+//        int x = 99;
+//        char c = (char) x;
+//        System.out.println(c);
+//        int y = 104;
+//        char h = (char) y;
+//        System.out.println(h);
+
+//        char c = 'c';
+////        int d = c;
+//        char a = (char) c;
+//        System.out.println(a);
+
+//        int a = 99;
+        char b = 99;
+        System.out.println(b);
+//        int c = 104;
+        char d = 104;
+        System.out.println(d);
+//        int e = 97;
+        char f = 97;
+        System.out.println(f);
+//        int x = 114;
+        char y = 114;
+        System.out.println(y);
+//        int z = 97;
+//        char a = (char) z;
+//        System.out.println(a);
+//        int f = 114;
+//        char r = (char) f;
+//        System.out.println(r);
+//        int a = 70;
+//        char b = (char) a; // casting
+//        Float x = 14.4f;
+//        double d = (double) x;
+//        System.out.println(c);
+//        System.out.println(((Object)c).getClass().getName());
+//        System.out.println(((Object)x).getClass().getName());
+//
+//        Integer obj1 = Integer.valueOf(10);
+//        String s = obj1.toString();
+//        System.out.println("s : " + s);
+//        Float obj2 = Float.valueOf(100.5f);
+//
+//        int y = obj1.intValue();
+//        float z = (int) obj2.floatValue();
+//        char c = (char) a;
+//        System.out.println(a);
+
+//        char c = 'c';
+//        int b = c;
+//        char k = (char) b;
+//        System.out.println(k);
+//        char h = 'h';
+//        int t = h;
+//        System.out.println(t);
+//        char a = 'a';
+//        int r = a;
+//        System.out.println(r);
+//        char u = 'r';
+//        int i = u;
+//        System.out.println(i);
+
+//        char c = 'c';
+//        System.out.println(c);
+//        char h = 104;
+//        System.out.println(h);
+//        char a = 97;
+//        System.out.println(a);
+//        char r = 114;
+//        System.out.println(r);
+
+//        99
+//104
+//97
+//114
+
     }
 }
